4bd7ff3670cc4a46dd95214c2c0e59e7ce05af08
Novejira Angela <156919887+Novejira@users.noreply.github.com>
2024-05-16 22:24:59 +0700

    add list more done

--- app/src/main/java/com/nafi/movflix/presentation/viewmore/ViewMoreActivity.kt
+++ app/src/main/java/com/nafi/movflix/presentation/viewmore/ViewMoreActivity.kt
@@ -13,7 +13,6 @@ import coil.load
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.nafi.movflix.R
 import com.nafi.movflix.data.model.Movie
-import com.nafi.movflix.data.source.network.model.movie.MovieListResponse
 import com.nafi.movflix.databinding.ActivityViewMoreBinding
 import com.nafi.movflix.databinding.SheetShareBinding
 import com.nafi.movflix.databinding.SheetViewBinding
